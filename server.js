d85c767dcb1a25fef842aa9221eacac012776347
Abriel Cleaver <abrielcleaver@gmail.com>
2022-03-20 14:29:15 -0700

    Add a few notes explaining res.send and tested sendStatus for http code 500. internal server error renders on page as expected and console shows status code was properly sent down to user

--- server.js
+++ server.js
@@ -4,9 +4,20 @@ const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 
-app.get('/', (req, res) => {
+// res and req are standard naming conventions
+// inside this function we run code whenever we try to access the url
+app.get("/", (req, res) => {
   console.log("here")
-  res.send("hi")
+  // to send data back to user we need to send something with the response 
+  // there are many methods to use, but the main one im using here is send
+  // self explanatory -- send down whatever we pass into it
+
+  // res.send("hi")
+  
+  // send is generic but not used very often, great for testing purposes
+
+  // generally will want to use something more specific like a status
+  res.sendStatus(500)
 })
 
 
