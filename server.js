606f216e0fd5a8c03e6fa5c763a7be33bf294c6c
Abriel Cleaver <abrielcleaver@gmail.com>
2022-03-20 14:55:01 -0700

    install ejs and adjust index file in views folder. hello rendering on page properly

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 
+app.set('view engine', 'ejs')
 app.get("/", (req, res) => {
   console.log("here");
 
   res.render('index')
-  //error given because there is no view engine setup
 })
 
 
