f384b8044dd3ff94b8530e38c51ac7683b89bccb
Abriel Cleaver <abrielcleaver@gmail.com>
2022-03-20 14:33:56 -0700

    Test out sending message with status code. ooops error message renders properly and console shows proper status code

--- server.js
+++ server.js
@@ -1,24 +1,15 @@
-// where all the server code goes yay!
-
 const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 
-// res and req are standard naming conventions
-// inside this function we run code whenever we try to access the url
 app.get("/", (req, res) => {
-  console.log("here")
+  console.log("here");
   // to send data back to user we need to send something with the response 
-  // there are many methods to use, but the main one im using here is send
-  // self explanatory -- send down whatever we pass into it
-
-  // res.send("hi")
-  
-  // send is generic but not used very often, great for testing purposes
 
   // generally will want to use something more specific like a status
-  res.sendStatus(500)
+  // for the user you would want to include a message -- check console to make sure status code still appears as it should
+  res.status(500).send("Oops... error");
 })
 
 
-app.listen(3000)
+app.listen(3000);
