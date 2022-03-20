3c6296baa8722064988b0ac9e04b2988d2074863
Abriel Cleaver <abrielcleaver@gmail.com>
2022-03-20 14:47:19 -0700

    Add views folder and index.html boilerplate

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ const app = express();
 app.get("/", (req, res) => {
   console.log("here");
 
-  // can call json method instead as well and the json code message is sent down properly
-  // using some type of api and want to send json to client? this is the best way to do so
-  res.download('server.js')
-// server.js was successfully downloaded
+  res.render('index')
+  //error given because there is no view engine setup
 })
 
 
