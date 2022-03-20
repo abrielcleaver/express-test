00e40e0d17e8edb1760753bf06da5a3dc1e9fa8d
Abriel Cleaver <abrielcleaver@gmail.com>
2022-03-20 14:17:26 -0700

    setup basic route with request and response. console log shows code is running properly and response shows data is being sent to user. hi is rendering on page

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 // where all the server code goes yay!
 
-console.log("hello")
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
+
+app.get('/', (req, res) => {
+  console.log("here")
+  res.send("hi")
+})
+
+
+app.listen(3000)
